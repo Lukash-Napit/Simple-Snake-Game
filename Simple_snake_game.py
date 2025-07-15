9112183d3318d3415f4abb65d17ed4863732bdf5
Lukash-Napit <lukashn159@gmail.com>
2025-07-15 22:48:04 +0545

    Update Simple_snake_game.py

--- Simple_snake_game.py
+++ Simple_snake_game.py
@@ -71,8 +71,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
-            self.high_score = int(data.read())
+        with open("High-score.txt") as hs:
+            self.high_score = int(hs.read())
         self.color("white")
         self.penup()
         self.goto(0, 215)
@@ -86,8 +86,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
+            with open("High-score.txt", mode="w") as hs:
+                hs.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
